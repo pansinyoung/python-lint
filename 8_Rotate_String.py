566362cebc747ed95ac24f06426e838a1d63d362
pansinyoung <pxydemail@gmail.com>
2019-09-08 21:38:46 -0500

    done

---
+++ 8_Rotate_String.py
@@ -0,0 +1,64 @@
+"""
+Given a string(Given in the way of char array) and an offset, rotate the string by offset in place. (rotate from left to right).
+
+Example
+Example 1:
+
+Input: str="abcdefg", offset = 3
+Output: str = "efgabcd"
+Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "efgabcd".
+Example 2:
+
+Input: str="abcdefg", offset = 0
+Output: str = "abcdefg"
+Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "abcdefg".
+Example 3:
+
+Input: str="abcdefg", offset = 1
+Output: str = "gabcdef"
+Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "gabcdef".
+Example 4:
+
+Input: str="abcdefg", offset =2
+Output: str = "fgabcde"
+Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "fgabcde".
+Example 5:
+
+Input: str="abcdefg", offset = 10
+Output: str = "efgabcd"
+Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "efgabcd".
+Challenge
+Rotate in-place with O(1) extra memory.
+
+Notice
+offset >= 0
+the length of str >= 0
+"""
+
+
+class Solution:
+    """
+    @param str: An array of char
+    @param offset: An integer
+    @return: nothing
+    """
+    def rotateString(self, s, offset):
+        if not s:
+            return s
+        offset = offset % len(s)
+        temp = s[len(s) - offset:] + s[:len(s) - offset]
+        s = temp
+        return s
+
+
+def main():
+    s = Solution()
+    print(s.rotateString('abcdefg', 3))
+    print(s.rotateString('abcdefg', 0))
+    print(s.rotateString('abcdefg', 1))
+    print(s.rotateString('abcdefg', 2))
+    print(s.rotateString('cppjavapy', 25))
+
+
+if __name__ == '__main__':
+    main()
